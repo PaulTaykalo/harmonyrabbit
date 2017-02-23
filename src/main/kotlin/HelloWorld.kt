0ee83a5f4b5a08249ccb676a73b1d87cb3053aa2
Michael Pustovit <mpustovit@stanfy.com.ua>
2017-02-23 21:01:57 +0200

    Fixed read data

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -2,16 +2,14 @@ import java.io.File
 import java.util.*
 
 fun main(args: Array<String>) {
-  println(readInputData(File("/Users/mih/prog/projects/harmonyrabbit/input.in")))
+  println(readInputData(File("input.in")))
 }
 
 fun readInputData(input: File): Input {
   val lines = input.readLines()
 
   val firstLine = lines[0].split(" ")
-//  val videosCount = firstLine[0].toInt()
   val endpointCount = firstLine[1].toLong()
-//  val requestDescCount = firstLine[2].toLong()
   val cachesCount = firstLine[3].toLong()
   val cachesSize = firstLine[4].toLong()
 
@@ -20,14 +18,14 @@ fun readInputData(input: File): Input {
   val endpoints: MutableList<Endpoint> = ArrayList()
 
   var currentLine = 2;
-  for (i in 0..endpointCount) {
+  for (i in 0..(endpointCount - 1)) {
     val endpointInfo = lines[currentLine].split(" ")
     currentLine++
 
     val endpoint = Endpoint(i, endpointInfo[0].toLong())
 
     val endpointCachesCount = endpointInfo[1].toLong()
-    for (j in 0..endpointCachesCount) {
+    for (j in 1..endpointCachesCount) {
       val cacheConnection = lines[currentLine].split(" ")
       endpoint.caches[cacheConnection[0].toLong()] = cacheConnection[1].toLong()
       currentLine++
@@ -36,19 +34,21 @@ fun readInputData(input: File): Input {
     endpoints.add(endpoint)
   }
 
+  val requests = ArrayList<Request>()
   while (currentLine < lines.size) {
     val curLine = lines[currentLine].split(" ")
+    currentLine++
 
     val video = curLine[0].toLong()
     val endpoint = curLine[1].toLong()
     val count = curLine[2].toLong()
 
-    endpoints[endpoint.toInt()].requests[video] = count
+    requests.add(Request(video, endpoint, count))
   }
 
-  return Input(endpoints, videos, cachesCount, cachesSize)
+  return Input(endpoints, videos, cachesCount, cachesSize, requests)
 }
 
-data class Endpoint(val id: Long, val dataCenterLatency: Long, val caches: MutableMap<Long, Long> = HashMap(),
-                    val requests: MutableMap<Long, Long> = HashMap())
-data class Input(val endpoints: List<Endpoint>, val videos: List<Long>, val cacheCount: Long, val cacheSize: Long)
+data class Endpoint(val id: Long, val dataCenterLatency: Long, val caches: MutableMap<Long, Long> = HashMap())
+data class Request(val video: Long, val endpoint: Long, val count: Long)
+data class Input(val endpoints: List<Endpoint>, val videos: List<Long>, val cacheCount: Long, val cacheSize: Long, val requests: List<Request>)
